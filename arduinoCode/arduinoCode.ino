99fab17559330bd3fbbbb220d3a3c01b49acf717
Neil Sawhney <Neil@neil-sawhney.com>
2023-03-16 22:53:14 -0400

    stuff

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,16 +1,42 @@
 #include <Arduino.h>
+#include <NewPing.h>
+#include <Servo.h>
 
-void setup()
-{
-	Serial.begin(9600);
-	Serial1.begin(230400);
+#define TRIGGER_PIN  9
+#define ECHO_PIN     10
+#define TRIGGER_PIN2  8
+#define ECHO_PIN2     7
+#define MAX_DISTANCE 100 // Maximum distance we want to measure (in centimeters).
+#define tol 5
+#define offset 10
+
+Servo ser;
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup() {
+  Serial.begin(9600);
+  ser.attach(5);
+  ser.write(90);
 }
 
-void loop()
-{
-	if (Serial1.available())
-	{
-		char data_rcvd = Serial1.read(); // read one byte from serial buffer and save to data_rcvd
-		Serial.write(data_rcvd); // write data_rcvd to serial monitor
-	}
+void loop() {
+  delay(50);                    // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+
+  float distance = sonar.ping_cm(); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  float distance2 = sonar2.ping_cm(); // Send ping, get distance in cm and print result (0 = outside set distance range)
+
+  Serial.println(String(distance) + "\t" + String(distance2) + " cm");
+  
+  if (distance > tol && distance2 <= tol){
+    ser.write(atan(distance/3)*180/PI - offset);
+  }
+  else if (distance <= tol && distance2 > tol){
+    ser.write(180 - atan(distance2/3)*180/PI + offset);
+  }
+  else if (distance > 0 && distance2 > tol){
+    ser.write(90);
+  }
 }
